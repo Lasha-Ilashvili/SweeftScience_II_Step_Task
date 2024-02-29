4ce8954e642cb2ba45c08941a712d9ffa324db67
Lasha-Ilashvili <ilashvililasha@gmail.com>
2024-02-29 22:02:47 +0400

    Problem 1 is done. Used kinda brut-force approach, will work for better solution

--- com/example/sweeftscience_II_step_task/Problem1.kt
+++ com/example/sweeftscience_II_step_task/Problem1.kt
@@ -1,7 +1,25 @@
 package com.example.sweeftscience_II_step_task
 
+/*
+ * Task 1
+ * გვაქვს მთელი რიცხვების ჩამონათვალი სადაც ერთის გარდა ყველა რიცხვი მეორდება, იპოვეთ ის
+ * რიცხვი რომელიც არ მეორდება. int singleNumber(int[] nums)
+ *
+ * *********************************************************
+*/
 
 
-fun main() {
 
+fun singleNumber(nums: IntArray): Int {
+    nums.sort()
+
+    for (i in 1..<nums.size step 2) {
+        if (nums[i] != nums[i - 1])
+            return nums[i - 1]
+
+        if (i + 1 == nums.lastIndex)
+            return nums[i + 1]
+    }
+
+    return nums[0]
 }
