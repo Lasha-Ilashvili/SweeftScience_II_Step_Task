e9e7402df187acd8b49f269ab549fab009dbf9ae
Lasha-Ilashvili <ilashvililasha@gmail.com>
2024-03-02 18:33:00 +0400

    Problem 3 is done

--- com/example/sweeftscience_II_step_task/Problem3.kt
+++ com/example/sweeftscience_II_step_task/Problem3.kt
@@ -1,7 +1,25 @@
 package com.example.sweeftscience_II_step_task
 
+/*
+ * Task 3
+ *
+ * მოცემულია მასივი, რომელიც შედგება სიტყვებისგან(string). დაწერეთ ფუნქცია
+ * რომელსაც გადაეცემა ეს მასივი და აბრუნებს ყველაზე გრძელ თავსართს(prefix) რომელიც
+ * მეორდება ამ სიტყვებში. თუ არცერთი არ მეორდება აბრუნებს ცარიელ
+ * სტრინგს.მაგ:[“extract”,”exhale”, “excavate”] , პასუხი იქნება “ex”
+ *
+ * *********************************************************
+*/
 
+/**
+ * Finds the longest common prefix among an array of strings.
+ * If there's no common prefix, an empty string is returned.
+ */
+fun longestPrefix(array: Array<String>): String {
 
-fun main() {
-
+    return array.reduceOrNull() { prefix, current ->
+        prefix.zip(current).takeWhile { (a, b) ->
+            a == b
+        }.joinToString("") { it.first.toString() }
+    } ?: ""
 }
